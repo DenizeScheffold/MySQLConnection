506e7e2196b984d5cdac0be9422173f62d21bbd5
DenizeScheffold <91418720+DenizeScheffold@users.noreply.github.com>
2022-02-07 16:06:05 +0100

    first commit

---
+++ mysqlConnection/src/main/java/se/denize/DemoApplication.java
@@ -0,0 +1,23 @@
+package se.denize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.denize.student.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(DemoApplication.class, args);
+        }
+
+
+
+    }
